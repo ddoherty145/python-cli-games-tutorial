8f0d262a611c8ebab01fd5c61eae640957eae257
David Doherty <froztydavie@Davids-MacBook-Air-3.local>
2024-09-14 10:46:40 -0700

    completed flash card tester

--- flash-cards/flashcards.py
+++ flash-cards/flashcards.py
@@ -1,6 +1,8 @@
 # json import
 import json
 
+def start_game(greetings)
+     
 # open the file and parse the JSON
 with open('me-capitals.json', 'r') as f:
     data = json.load(f)
@@ -19,9 +21,10 @@ for i in data["cards"]:
         print("Incorrect! The correct answer was", i["a"])
         print(f"Current Score: {score}/{total}")
 
-
+#returns final score and tells you how many questions you missed
 if score == total:
         print(f"Congraulations! You got all questions correct!!")
 
 else:
         print(f"Too bad you missed {total - score} better luck next time!")
+
